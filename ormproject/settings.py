308886ae93189d2cb898735fc0c7a7746b1760ba
Alexis Hadjipanayis <93918531+ahadjipanayis@users.noreply.github.com>
2024-09-12 15:44:16 +0300

    ok

--- ormproject/settings.py
+++ ormproject/settings.py
@@ -389,7 +389,7 @@ JAZZMIN_SETTINGS = {
     # Related Modal #
     #################
     # Use modals instead of popups
-    "related_modal_active": True,
+    "related_modal_active": False,
 
     #############
     # UI Tweaks #
